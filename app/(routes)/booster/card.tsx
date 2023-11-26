83e3eeea2d7cddbdc99950b034731a12c3f94830
KlayFrog <105149475+KlayFrog@users.noreply.github.com>
2023-11-26 12:17:00 +0100

    Update card.tsx

--- app/(routes)/booster/card.tsx
+++ app/(routes)/booster/card.tsx
@@ -9,7 +9,7 @@ import { on } from "events";
 
 export type BoosterCardProps = {
  card: CardType;
- onFlip?: (isFlipped: boolean) => void;
+ onFlip: (cardKey: string, isFlipped: boolean) => void;
 };
 
 export const BoosterCard = ({ card, onFlip }: BoosterCardProps) => {
@@ -17,55 +17,55 @@ export const BoosterCard = ({ card, onFlip }: BoosterCardProps) => {
  const [isFlipped, setIsFlipped] = useState(false);
 
  const handleFlip = () => {
-   setDisplayedSide("front");
-   setIsFlipped(true);
-   onFlip?.(isFlipped);
+  setDisplayedSide("front");
+  setIsFlipped(true);
+  onFlip(card.key, isFlipped);
  };
 
  return (
-   <div
-     className={cn(
-       "w-[128px] h-[96px] relative transition duration-500",
-       styles.cardContainer,
-       {
-         [styles.cardShowBack]: displayedSide === "back",
-       }
-     )}
-     onClick={handleFlip}
-   >
-     <Image
-       src={getCardFrontImageUrl(card)}
-       alt={`Card with id ${card.id}`}
-       width={128}
-       height={96}
-       className={cn("pixelated absolute inset-0", styles.cardFront)}
-     />
-     <Image
-       src={getCardBackImageUrl(card)}
-       alt={`Card with id ${card.id}`}
-       width={128}
-       height={96}
-       className={cn("pixelated absolute inset-0", styles.cardBack)}
-     />
-   </div>
+  <div
+    className={cn(
+      "w-[128px] h-[96px] relative transition duration-500",
+      styles.cardContainer,
+      {
+        [styles.cardShowBack]: displayedSide === "back",
+      }
+    )}
+    onClick={handleFlip}
+  >
+    <Image
+      src={getCardFrontImageUrl(card)}
+      alt={`Card with id ${card.id}`}
+      width={128}
+      height={96}
+      className={cn("pixelated absolute inset-0", styles.cardFront)}
+    />
+    <Image
+      src={getCardBackImageUrl(card)}
+      alt={`Card with id ${card.id}`}
+      width={128}
+      height={96}
+      className={cn("pixelated absolute inset-0", styles.cardBack)}
+    />
+  </div>
  );
 };
 
 
 const getCardImageDirectory = (card: CardType) => {
-  switch (card.category) {
-    case "common":
-      return `/images/cards/common`;
-    case "energy":
-      return `/images/cards/energy`;
-    case "special":
-    default:
-      return `/images/cards/special`;
-  }
+ switch (card.category) {
+   case "common":
+     return `/images/cards/common`;
+   case "energy":
+     return `/images/cards/energy`;
+   case "special":
+   default:
+     return `/images/cards/special`;
+ }
 };
 
 const getCardFrontImageUrl = (card: CardType) =>
-  `${getCardImageDirectory(card)}/${card.id}.png`;
+ `${getCardImageDirectory(card)}/${card.id}.png`;
 
 const getCardBackImageUrl = (card: CardType) =>
-  `${getCardImageDirectory(card)}/back.png`;
+ `${getCardImageDirectory(card)}/back.png`;
