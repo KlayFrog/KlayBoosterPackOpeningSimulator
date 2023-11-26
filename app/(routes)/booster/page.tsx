fdbd22ca85aa1bfd16eb0e4f497c87f2c5cefda2
KlayFrog <105149475+KlayFrog@users.noreply.github.com>
2023-11-26 11:46:52 +0100

    Update page.tsx

--- app/(routes)/booster/page.tsx
+++ app/(routes)/booster/page.tsx
@@ -16,16 +16,16 @@ type BoosterAction = { type: "flipCard" } | { type: "openNewPack" };
 
 function reducer(state: BoosterState, action: BoosterAction): BoosterState {
  switch (action.type) {
-   case "flipCard":
-     return { ...state, oneCardFlipped: true };
-   case "openNewPack":
-     return {
-       ...state,
-       cards: getRandomBooster().map((card) => ({ ...card, key: uuidv4() })),
-       oneCardFlipped: false,
-     };
-   default:
-     throw new Error();
+  case "flipCard":
+    return { ...state, oneCardFlipped: true };
+  case "openNewPack":
+    return {
+      ...state,
+      cards: getRandomBooster().map((card) => ({ ...card, key: uuidv4() })),
+      oneCardFlipped: false,
+    };
+  default:
+    throw new Error();
  }
 }
 
@@ -38,32 +38,32 @@ export default function BoosterIndex() {
  const [state, dispatch] = useReducer(reducer, initialState);
 
  const handleCardFlip = (isFlipped: boolean) => {
-   if (isFlipped) {
-     dispatch({ type: "flipCard" });
-   }
+  if (isFlipped && state.cards.every(card => card.isFlipped)) {
+    dispatch({ type: "flipCard" });
+  }
  };
 
  return (
-   <main className="w-full h-full flex flex-col justify-center items-center gap-3">
-     <div className="flex flex-wrap gap-3 max-w-[600px] items-center justify-center">
-       {state.cards.map((card) => (
-         <BoosterCard
-           key={card.key}
-           card={card}
-           onFlip={handleCardFlip}
-         />
-       ))}
-     </div>
-     <Button
-       onClick={() =>
-         dispatch({
-           type: "openNewPack",
-         })
-       }
-       disabled={!state.oneCardFlipped}
-     >
-       Open a new pack
-     </Button>
-   </main>
+  <main className="w-full h-full flex flex-col justify-center items-center gap-3">
+    <div className="flex flex-wrap gap-3 max-w-[600px] items-center justify-center">
+      {state.cards.map((card) => (
+        <BoosterCard
+          key={card.key}
+          card={card}
+          onFlip={handleCardFlip}
+        />
+      ))}
+    </div>
+    <Button
+      onClick={() =>
+        dispatch({
+          type: "openNewPack",
+        })
+      }
+      disabled={!state.oneCardFlipped}
+    >
+      Open a new pack
+    </Button>
+  </main>
  );
 }
