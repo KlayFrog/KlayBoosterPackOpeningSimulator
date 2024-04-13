1a1311c392b90caae27514b11d08218f373b8d72
KlayFrog <105149475+KlayFrog@users.noreply.github.com>
2024-04-13 13:04:25 +0200

    Kingler

--- app/(routes)/booster/card.data.ts
+++ app/(routes)/booster/card.data.ts
@@ -6,11 +6,11 @@ const ENERGY_CARDS: CardType[] = Array.from({ length: 10 }, (_, i) => ({
   id: i + 1,
   category: "energy",
 }));
-const SPECIAL_CARDS: CardType[] = Array.from({ length: 32 }, (_, i) => ({
+const SPECIAL_CARDS: CardType[] = Array.from({ length: 33 }, (_, i) => ({
   category: "special",
   id: i + 1,
 }));
-const COMMON_CARDS: CardType[] = Array.from({ length: 40 }, (_, i) => ({
+const COMMON_CARDS: CardType[] = Array.from({ length: 41 }, (_, i) => ({
   category: "common",
   id: i + 1,
 }));
