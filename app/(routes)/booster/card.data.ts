f78de14350a3594a1b43eee1020f7be5cc8dfe57
KlayFrog <105149475+KlayFrog@users.noreply.github.com>
2025-07-21 10:54:02 +0200

    drowzee

--- app/(routes)/booster/card.data.ts
+++ app/(routes)/booster/card.data.ts
@@ -6,7 +6,7 @@ const ENERGY_CARDS: CardType[] = Array.from({ length: 10 }, (_, i) => ({
   id: i + 1,
   category: "energy",
 }));
-const COMMON_CARDS: CardType[] = Array.from({ length: 58 }, (_, i) => ({
+const COMMON_CARDS: CardType[] = Array.from({ length: 59 }, (_, i) => ({
   category: "common",
   id: i + 1,
 }));
@@ -14,7 +14,7 @@ const COMMONSHINY_CARDS: CardType[] = Array.from({ length: 10 }, (_, i) => ({
   category: "commonshiny",
   id: i + 1,
 }));
-const LOWRES_CARDS: CardType[] = Array.from({ length: 51}, (_, i) => ({
+const LOWRES_CARDS: CardType[] = Array.from({ length: 52}, (_, i) => ({
   category: "lowres",
   id: i + 1,
 }));
